2f2255711ac3020649b896637bac707d65d835aa
chen <cm1191592314@163.com>
2024-09-27 12:07:20 +0800

    初始增删查改

--- src/main/java/com/chen/InterviewAce/controller/UserController.java
+++ src/main/java/com/chen/InterviewAce/controller/UserController.java
@@ -44,9 +44,6 @@ import static com.chen.InterviewAce.service.impl.UserServiceImpl.SALT;
 
 /**
  * 用户接口
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @RestController
 @RequestMapping("/user")
